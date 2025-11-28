e3d6e935cdde89538d14c2accc89eb0de5ac7ab8
Janderson1924 <justinanderson101@gmail.com>
2025-11-28 02:38:51 -0700

    added state function for guessed letters

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import words from './wordList.json'
 import { useState } from 'react'
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState(() => {
+  const [wordToGuess, setWordToGuess] = useState<string>(() => {
     return words[Math.floor(Math.random() * words.length)]
   })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+  console.log(guessedLetters)
   console.log(wordToGuess)
 
   return <h1>Hello World</h1>
