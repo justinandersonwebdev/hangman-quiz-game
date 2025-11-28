bd2d1db6b233d1f70ba9c64a3f23859fa7d86653
Janderson1924 <justinanderson101@gmail.com>
2025-11-28 02:29:45 -0700

    added word-list, state function for selecting a word

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
+import words from './wordList.json'
+import { useState } from 'react'
+
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+  console.log(wordToGuess)
+
   return <h1>Hello World</h1>
 }
 
