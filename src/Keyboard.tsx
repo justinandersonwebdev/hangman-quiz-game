e01c12c5e4fd8cfb6ef70b783c80bb11bcc81b53
Janderson1924 <justinanderson.webdev@gmail.com>
2025-12-03 02:35:01 -0700

    build Keyboard component, unstyled

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,1 +1,43 @@
-export function Keyboard() {}
+const letterList: string[] = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+]
+export function Keyboard() {
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fit, minmax(75px, 1fr))',
+        gap: '.5rem'
+      }}
+    >
+      {letterList.map((letter) => {
+        return <button key={letter}>{letter}</button>
+      })}
+    </div>
+  )
+}
