f2180f8bb1e2687c972bfe583a2ba148fa887343
Janderson1924 <justinanderson.webdev@gmail.com>
2025-12-03 02:13:37 -0700

    build HangmanWord component, add functional logic for guessed word/letters

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,1 +1,35 @@
-export function HangmanWord() {}
+export function HangmanWord() {
+  const word: string = 'test'
+  const guessedLetters: string[] = ['t', 'e']
+  return (
+    <div
+      style={{
+        display: 'flex',
+        gap: '.25em',
+        fontSize: '6rem',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontFamily: 'monospace'
+      }}
+    >
+      {word.split('').map(
+        (letter, index) => (
+          console.log(letter),
+          (
+            <span key={index} style={{ borderBottom: '.1em solid black' }}>
+              <span
+                style={{
+                  visibility: guessedLetters.includes(letter)
+                    ? 'visible'
+                    : 'hidden'
+                }}
+              >
+                {letter}
+              </span>
+            </span>
+          )
+        )
+      )}
+    </div>
+  )
+}
